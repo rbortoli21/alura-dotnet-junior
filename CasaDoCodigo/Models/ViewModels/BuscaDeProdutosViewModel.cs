9b2ca8a6e7ece21abf2cd1d99690e72bd7dbc10f
Rafael Bortoli <rafaelbortoli21@gmail.com>
2022-05-15 12:53:54 -0300

    Criando view model de BuscaDeProdutos.cshtml

---
+++ CasaDoCodigo/Models/ViewModels/BuscaDeProdutosViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace CasaDoCodigo.Models.ViewModels
+{
+    public class BuscaDeProdutosViewModel
+    {
+        public IList<Categoria> Categorias { get; set; }
+        public IList<Produto> Produtos { get; set; }
+        public BuscaDeProdutosViewModel(IList<Produto> produtos, IList<Categoria> categorias) 
+        {
+           Categorias = categorias; 
+           Produtos = produtos;   
+        }
+       
+    }
+}
